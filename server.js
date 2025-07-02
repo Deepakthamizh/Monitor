b9a50ce1337ed598b0bcbfe41aecfb978ee260f4
Deepakthamizh <deepak.thamizh@zohocorp.com>
2025-07-02 10:54:35 +0530

    port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app'); //importing the app.js file
 
 const db = require('./config/db'); //importing the db.js
 
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 app.listen(port,()=>{
   console.log(`Server listening on port http://localhost:${port}`);
