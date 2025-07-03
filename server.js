f6e9c847ede94addabdedf9539cc8b3077c0d8c0
Deepakthamizh <deepak.thamizh@zohocorp.com>
2025-07-03 23:09:20 +0530

    signup bug fix - 2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const app = require('./app'); //importing the app.js file
 
 const db = require('./config/db'); //importing the db.js
 
+const {userModel, collection} =require('./model/user.data.js'); 
+
 const port = process.env.PORT || 5001;
 
 app.listen(port,()=>{
