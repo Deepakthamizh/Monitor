54c4e41dd16d42a43eea333465711aa296ee1a7d
Deepakthamizh <deepak.thamizh@zohocorp.com>
2025-07-04 14:35:00 +0530

    comment firebaseRoute.js - 2

--- firebaseRoute.js
+++ firebaseRoute.js
@@ -1,6 +1,6 @@
-// const express = require("express");
-// const router = express.Router();
-// const { collection } = require("./model/user.data");
+const express = require("express");
+const router = express.Router();
+const { collection } = require("./model/user.data");
 
 // router.post("/firebase-login", async (req, res) => {
 //   const { firebaseUID } = req.body;
